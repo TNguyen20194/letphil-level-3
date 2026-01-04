7aa1cdb016ab7232c8b32442cc5952618b44758a
Truong Nguyen <truong.nguyen201@gmail.com>
2026-01-04 13:17:29 -0500

    Begin Body section

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,3 +1,47 @@
+type Cta = {
+  label: string,
+  href: string
+};
+
+interface ImagesInterface {
+    src: string;
+    alt: string;
+}
+
+interface BodyProps {
+  iphoneBanner: string,
+  appleWatchBanner: string,
+  ipadAirBanner: string,
+  
+  airpodPro: string,
+  appleCard: string,
+  fitnessCard: string,
+  macbookPro: string,
+};
+interface HeroProps {
+  title: string,
+  subtitle: string,
+  banner: ImagesInterface,
+  ctas: Cta[]
+}
+
+function Hero({ title, subtitle, banner, ctas }: HeroProps) {
+  return (
+    <section>
+      <div>
+        {/* Title */}
+        <h2></h2>
+        {/* Subtitle */}
+        <p></p>
+        {/* CTAs */}
+        <div></div>
+        {/* Banner */}
+        <div></div>
+      </div>
+    </section>
+  )
+} 
+
 function Body() {
   return (
      <div className="card">
