fd5a9790cb3c459725765696edb0912bad463c43
Truong Nguyen <truong.nguyen201@gmail.com>
2026-01-11 18:59:09 -0500

    Update footer structure

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,13 +3,13 @@ type FooterLink = {
   href: string,
 }
 
-type FooterColumn = {
+type FooterGroup = {
   title: string,
-  links: FooterLink
+  links: FooterLink[]
 }
 
-interface FooterProps {
-
+type FooterColumn = {
+  groups : FooterGroup[]
 }
 
 function Footer() {
@@ -19,18 +19,144 @@ function Footer() {
   );
 }
 
-const footerLinks = [
-
-  {title: "Stop and Learn",
-  itsm: ["Store", "Mac", "iPad", "iPhone", "Watch", "Vision", "AirPods", "TV & Home", "AirTag", "Accessories", "Gift Cards"]
+const columns:FooterColumn[] = [
+  {
+    groups: [
+      {
+         title: "Stop and Learn",
+         links: [
+          {label: "Store", href: "#"},
+          {label: "Mac", href: "#"},
+          {label: "iPad", href: "#"},
+          {label: "iPhone", href: "#"},
+          {label: "Watch", href: "#"},
+          {label: "Vision", href: "#"},
+          {label: "AirPods", href: "#"},
+          {label: "TV & Home", href: "#"},
+          {label: "AirTag", href: "#"},
+          {label: "Accessories", href: "#"},
+          {label: "Gift Cards", href: "#"},
+        ],
+      },
+      {
+         title: "Apple Wallet",
+         links: [
+          {label: "Wallet", href: "#"},
+          {label: "Apple Card", href: "#"},
+          {label: "Apple Pay", href: "#"},
+          {label: "Apple Cash", href: "#"},
+        ]
+      },
+    ]
   },
-  {title: "Account",
-  itsm: ["Manage Your Apple Account", "Apple Store Account", "iCloud.com"]
+  {
+    groups: [
+      {
+         title: "Account",
+         links: [
+          {label: "Manage Your Apple Account", href: "#"},
+          {label: "Apple Store Account", href: "#"},
+          {label: "iCloud.com", href: "#"},
+        ],
+      },
+      {
+         title: "Entertainment",
+         links: [
+          {label: "Apple One", href: "#"},
+          {label: "Apple tV", href: "#"},
+          {label: "Apple Music", href: "#"},
+          {label: "Apple Arcade", href: "#"},
+          {label: "Apple Fitness+", href: "#"},
+          {label: "Apple News+", href: "#"},
+          {label: "Apple Podcasts", href: "#"},
+          {label: "Apple Books", href: "#"},
+          {label: "Apple Store", href: "#"},
+        ]
+      },
+    ]
   },
-  {title: "Apple Store",
-  itsm: ["Find a Store", "Genius Bar", "Today at Apple", "Group Reservations", "Apple Camp", "Apple Store App", "Certified Refurbished", "Apple Trade In", "Financing", "Carrier Deals at Apple", "Order Status", "Shopping Help"]
+  {
+    groups: [
+      {
+         title: "Apple Store",
+         links: [
+          {label: "Find a Store", href: "#"},
+          {label: "Genius Bar", href: "#"},
+          {label: "Group Reservations", href: "#"},
+          {label: "Apple Camp", href: "#"},
+          {label: "Apple Store App", href: "#"},
+          {label: "Certified Refurbished", href: "#"},
+          {label: "Apple Trade In", href: "#"},
+          {label: "Financing", href: "#"},
+          {label: "Carrier Deals at Apple", href: "#"},
+          {label: "Order Status", href: "#"},
+          {label: "Shopping Help", href: "#"},
+        ],
+      },
+    ]
   },
-
+  {
+    groups: [
+      {
+         title: "For Business",
+         links: [
+          {label: "Apple and Business", href: "#"},
+          {label: "Shop for Business", href: "#"},
+        ],
+      },
+      {
+         title: "For Education",
+         links: [
+          {label: "Apple and Education", href: "#"},
+          {label: "Shop for K-12", href: "#"},
+          {label: "Shop for College", href: "#"},
+        ],
+      },
+      {
+         title: "For Healthcare",
+         links: [
+          {label: "Apple and Healthcare", href: "#"},
+          {label: "Shop for Business", href: "#"},
+        ],
+      },
+      {
+         title: "For Government",
+         links: [
+          {label: "Apple and Government", href: "#"},
+          {label: "Shop for Veterans and Military", href: "#"},
+          {label: "Shop for State and Local Employees", href: "#"},
+          {label: "Shop for Federal Employees", href: "#"},
+        ],
+      },
+    ]
+  },
+  {
+    groups: [
+      {
+         title: "Apple Values",
+         links: [
+          {label: "Accessibility", href: "#"},
+          {label: "Education", href: "#"},
+          {label: "Environment", href: "#"},
+          {label: "Inclusion and Diversity", href: "#"},
+          {label: "Racial Equity and Justice", href: "#"},
+          {label: "Supply Chain Innovation", href: "#"},
+        ],
+      },
+      {
+         title: "About Apple",
+         links: [
+          {label: "Newsroom", href: "#"},
+          {label: "Apple Leadership", href: "#"},
+          {label: "Career Opportunities", href: "#"},
+          {label: "Investors", href: "#"},
+          {label: "Ethics & Compliance", href: "#"},
+          {label: "Events", href: "#"},
+          {label: "Contact Apple", href: "#"},
+        ],
+      },
+    ]
+  },
 ]
 
 export default Footer;
