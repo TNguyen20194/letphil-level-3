7903b149ffda489785a6ccdbb61f236bb9aa361b
Truong Nguyen <truong.nguyen201@gmail.com>
2026-01-11 11:18:31 -0500

    Update Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,37 @@
+type FooterLink = {
+  label: string,
+  href: string,
+}
+
+type FooterColumn = {
+  title: string,
+  links: FooterLink
+}
+
+interface FooterProps {
+
+}
+
 function Footer() {
   return (
-     <div cla ssName="card">
+     <div className="card">
       </div>
   );
 }
+
+const footerLinks = [
+
+  {title: "Stop and Learn",
+  itsm: ["Store", "Mac", "iPad", "iPhone", "Watch", "Vision", "AirPods", "TV & Home", "AirTag", "Accessories", "Gift Cards"]
+  },
+  {title: "Account",
+  itsm: ["Manage Your Apple Account", "Apple Store Account", "iCloud.com"]
+  },
+  {title: "Apple Store",
+  itsm: ["Find a Store", "Genius Bar", "Today at Apple", "Group Reservations", "Apple Camp", "Apple Store App", "Certified Refurbished", "Apple Trade In", "Financing", "Carrier Deals at Apple", "Order Status", "Shopping Help"]
+  },
+
+]
 
 export default Footer;
 
